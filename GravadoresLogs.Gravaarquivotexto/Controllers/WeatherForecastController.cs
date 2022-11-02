f32ef77d2e2b77d7fc3476c4bfbf705ecbf49ffa
Carlos Oliveira <carlossoliveiraa@hotmail.com>
2022-11-02 16:07:00 -0300

    serilog c# com blob storage

---
+++ GravadoresLogs.Gravaarquivotexto/Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+ 
+
+namespace GravadoresLogs.Gravaarquivotexto.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    { 
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+
+            _logger.LogWarning("Exemplo de log de alerta");
+            _logger.LogError(new Exception(), "Exemplo de exception");
+            _logger.LogInformation("Exemplo de logs de informações");
+
+            var rng = new Random();
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            _logger.LogInformation("Total de arquivos gerados:", result.Count().ToString());
+
+            return result;
+        }
+    }
+}
